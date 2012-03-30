29dcd40a07d3714ecb23f39cfe74c256d49fa780
Luis Urrea <lfurrea@simplecs.net>
2012-03-29 19:00:27 -0600

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                           "Luis Urrea"
+    email                           "lfurrea@example.com"
+    password                     "foobar"
+    password_confirmation "foobar"
+  end
+end
