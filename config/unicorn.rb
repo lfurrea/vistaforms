e2ef2c1f0debb5788c33339bcb1f583599cf8466
Luis Urrea <lfurrea@simplecs.net>
2012-05-08 20:39:02 -0600

    deployment configs

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/deployer/apps/sample_app"
+pid "/home/deployer/apps/sample_app/tmp/pids/unicorn.pid"
+stderr_path "/home/deployer/apps/sample_app/log/unicorn.log"
+stdout_path "/home/deployer/apps/sample_app/log/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 2
+timeout 30
