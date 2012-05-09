1d70bec6adf829e0b993507368ffbc51aa93f602
Luis Urrea <lfurrea@simplecs.net>
2012-05-09 15:37:25 -0600

    Changed unicorn.rb to reflect proper working_directory for Capistrano deployment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 working_directory "/home/deployer/apps/sample_app/current"
-pid "/home/deployer/apps/sample_app/tmp/pids/unicorn.pid"
-stderr_path "/home/deployer/apps/sample_app/log/unicorn.log"
-stdout_path "/home/deployer/apps/sample_app/log/unicorn.log"
+pid "/home/deployer/apps/sample_app/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deployer/apps/sample_app/current/log/unicorn.log"
+stdout_path "/home/deployer/apps/sample_app/current/log/unicorn.log"
 
 listen "/tmp/unicorn.todo.sock"
 worker_processes 2
