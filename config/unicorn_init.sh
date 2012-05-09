2d2854e3040ca1c555a38209e4852f0b01253e89
Luis Urrea <lfurrea@simplecs.net>
2012-05-08 23:33:13 -0600

    Fixed unicorn_init.sh unicorn.rc path

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -5,9 +5,9 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/sample_app
+APP_ROOT=/home/deployer/apps/sample_app/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="$APP_ROOT/current/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
 set -u
 
